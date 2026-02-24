1647f632f6f50603f439c0bf19b645366376e175
buchm074 <buchm074@umn.edu>
2026-02-24 17:57:28 -0600

    initial commit

---
+++ p3a.r
@@ -0,0 +1,8 @@
+b 1 : a 1 b 1 : 0.01
+a 1 Ylog 2 : a 1 Yplog 1 L 1 : 50
+a 1 : : 50
+Yplog 1 : Ylog 1 : 1
+L 1 : e 1 : 0.01
+e 1 X 1 : e 1 X 1 Xwork 1 : 50
+e 1 Xwork 1 : e 1 Z 1 : 50
+e 1 : : 250
---
+++ p3b.r
@@ -0,0 +1,8 @@
+b 1 : a 1 b 1 : 0.01
+a 1 Xlog 2 : a 1 Xplog 1 L 1 : 50
+a 1 : : 50
+Xplog 1 : Xlog 1 : 1
+L 1 : e 1 : 0.01
+e 1 Y 1 : espent 1 Yp 2 : 50
+espent 1 : : 50
+Yp 1 : Y 1 : 1
